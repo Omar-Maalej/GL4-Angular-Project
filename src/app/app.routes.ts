9322bb9164a0845ec6c3043a93e659febf6bfb00
Omar-Maalej <omar.maalej@insat.ucar.tn>
2025-01-17 12:11:06 +0100

    project structure and add tailwindcss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { ShopComponent } from './shop/shop.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : '', component : HomeComponent},
+  {path : 'contact', component : ContactComponent},
+  {path : 'blog', component : BlogComponent},
+  {path : 'shop', component : ShopComponent}, 
+  {path : '**',  redirectTo : ''}
+];
