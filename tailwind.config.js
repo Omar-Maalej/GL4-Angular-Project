c48d2496a81184c58e0a8173ecb3a636a6ea849f
ElyesBelgouthi <belelyesbel@gmail.com>
2025-01-17 17:50:42 +0100

    Adding new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Titillium Web", "sans-serif"], // For titles and headers
+        body: ["Roboto", "sans-serif"], // For paragraphs and general text
+      },
+    },
   },
   plugins: [],
-}
+};
