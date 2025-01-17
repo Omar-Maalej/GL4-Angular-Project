0d3aabbcdd62f6ec4ddc0f9d42da8327bb915a69
Omar-Maalej <omar.maalej@insat.ucar.tn>
2025-01-17 16:54:02 +0100

    feat: placeholder navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  standalone : true
 })
 export class HeaderComponent {
+  isLoggedIn : boolean = false;
+  constructor(private router : Router){}
+
+  toggleLogin(){
+    this.isLoggedIn = !this.isLoggedIn;
+  }
 
 }
