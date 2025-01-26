7615c3a629acce248a8549526f7d235c5d88149e
Omar-Maalej <omar.maalej@insat.ucar.tn>
2025-01-26 18:29:36 +0100

    feat: add toastr and save cartItems in localstorage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CartItem } from '../models/cartItem';
 import { BehaviorSubject } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  private cartItems = new BehaviorSubject<CartItem[]>([]);
+  private cartItems = new BehaviorSubject<CartItem[]>(localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems') as string) : []);
   cartItems$ = this.cartItems.asObservable();
 
-  constructor() {}
+  constructor(private toastr : ToastrService, private router : Router) {}
 
   addToCart(product: CartItem) {
     const currentItems = this.cartItems.getValue();
@@ -21,12 +23,31 @@ export class CartService {
       currentItems.push({ ...product, quantity: 1 });
     }
 
+    localStorage.setItem('cartItems', JSON.stringify(currentItems));
+
+    const toast = this.toastr.success(
+      `${product.quantity} x [ ${product.name} ] added! Click To View Cart`,
+      'Product Added'
+    );
+
+    toast.onTap.subscribe(() => {
+      this.router.navigate(['/cart']);
+    });
+
     this.cartItems.next(currentItems);
   }
 
   removeFromCart(productId: number) {
+    const productName = this.cartItems.getValue().find(item => item.prodId === productId)?.name;
     const currentItems = this.cartItems.getValue().filter(item => item.prodId !== productId);
+
+    localStorage.setItem('cartItems', JSON.stringify(currentItems));
     this.cartItems.next(currentItems);
+
+    const toast = this.toastr.error(
+      `[ ${productName} ] removed!`,
+      'Product Removed'
+    );
   }
 
   updateQuantity(productId: number, quantity: number) {
@@ -35,6 +56,7 @@ export class CartService {
 
     if (itemToUpdate) {
       itemToUpdate.quantity = quantity;
+      localStorage.setItem('cartItems', JSON.stringify(currentItems));
       this.cartItems.next(currentItems);
     }
   }
@@ -44,6 +66,7 @@ export class CartService {
   }
 
   clearCart() {
+    localStorage.removeItem('cartItems');
     this.cartItems.next([]);
   }
 }
