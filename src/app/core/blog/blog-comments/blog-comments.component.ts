cbaa154fc671180eb64c2bb5eeec435f6206e831
samer-mansouri <samer.mansouri@insat.ucar.tn>
2025-01-27 18:33:18 +0100

    feat: comment only if logged in

--- src/app/core/blog/blog-comments/blog-comments.component.ts
+++ src/app/core/blog/blog-comments/blog-comments.component.ts
@@ -3,6 +3,10 @@ import { Comment } from '../../../models/comment.model';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BlogService } from '../../../services/blog.service';
+import { Store } from '@ngrx/store';
+import { AuthState } from '../../../store/auth/auth.state';
+import { Observable } from 'rxjs';
+import { selectIsLoggedIn } from '../../../store/auth/auth.selectors';
 
 @Component({
   selector: 'app-blog-comments',
@@ -17,9 +21,15 @@ export class BlogCommentsComponent {
     @Input() postId: number = 0;
     comment: string = '';
 
+    isLoggedIn$: Observable<AuthState['isLoggedIn']>;
+    
+
     constructor(
       private blogService: BlogService,
-    ) {}
+      private store: Store<{ auth: AuthState }>
+    ) {
+      this.isLoggedIn$ = this.store.select(selectIsLoggedIn);
+    }
 
     ngOnChanges(): void {};   
 
